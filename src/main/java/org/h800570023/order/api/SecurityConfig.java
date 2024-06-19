0fb66f074f0cd4495f621586a24887146d48dc55
andytsai <wa.addi@gmail.com>
2024-06-19 21:14:08 +0800

    加入line通知

--- src/main/java/org/h800570023/order/api/SecurityConfig.java
+++ src/main/java/org/h800570023/order/api/SecurityConfig.java
@@ -49,12 +49,11 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://6281-118-150-214-161.ngrok-free.app","http://localhost:3000","" +
-                                        "http://localhost:8000" ,
-                                "http://localhost:8080" ,
-                                "https://convincing-brandy-andytsia-d4b231f3.koyeb.app","https://e6be-118-150-214-161.ngrok-free.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
